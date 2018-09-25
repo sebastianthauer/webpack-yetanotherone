aba05b677558c2ca586cb04ca0b326ec8092cd5a
sebastianthauer <sebastianthauer@gmail.com>
2018-09-25 14:23:11 +0200

    test

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+   entry: ['.src/scss/main.scss'],
+
+   module: {
+       rules: [
+           /*
+           your other rules for JavaScript transpiling go in here
+           */
+           { // css / sass / scss loader for webpack
+               test: /\.(css|sass|scss)$/,
+               use: ExtractTextPlugin.extract({
+                   use: ['css-loader', 'sass-loader'],
+               })
+           }
+       ]
+   },
+   plugins: [
+       new ExtractTextPlugin({ // define where to save the file
+           filename: '../styles/style.css',
+           allChunks: true,
+       }),
+   ],
+};
